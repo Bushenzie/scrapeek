3157e7bffc3d456d352fdcb3d7f56456c9c86f45
Daniel Buchal <daniel.buchal01@gmail.com>
2025-08-26 07:18:34 +0200

    feat: Added logger

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 import { env } from "@/lib/env.ts";
 import { errorHandler } from "@/middlewares/error-handler.ts";
 import blueprintRoutes from "@/routes/blueprints/blueprints.routes.ts";
@@ -9,6 +10,7 @@ import runnerRoutes from "@/routes/runners/runners.routes.ts";
 const app = new Hono();
 
 app.onError(errorHandler);
+app.use(logger());
 app.use("/api/*", cors());
 
 app
