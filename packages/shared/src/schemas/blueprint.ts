234c1344ebb7f93778ef215cc4bc3043edeabb16
Daniel Buchal <daniel.buchal01@gmail.com>
2025-09-29 12:22:18 +0200

    fix: userId type

--- packages/shared/src/schemas/blueprint.ts
+++ packages/shared/src/schemas/blueprint.ts
@@ -105,7 +105,7 @@ const baseBlueprintSchema = z.object({
   url: z.url("Provided string must be URL").max(255),
   baseUrl: z.url("Provided string must be URL").max(255),
   name: z.string().min(3, "Minimum length of name is 3 chars"),
-  userId: z.uuid(),
+  userId: z.string().min(32).max(32),
   result: z
     .object({
       id: z.uuid(),
