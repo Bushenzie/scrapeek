7cd1b4c1a5ad970cc4a9fb7aafa8278d03af8472
Daniel Buchal <daniel.buchal01@gmail.com>
2025-10-01 11:55:49 +0200

    feat: Table component

---
+++ apps/client/src/components/ui/table/table.tsx
@@ -0,0 +1,112 @@
+import { type FC } from "react";
+import { cn } from "@/lib/utils/utils";
+import type {
+  TableBodyProps,
+  TableCaptionProps,
+  TableCellProps,
+  TableFooterProps,
+  TableHeaderProps,
+  TableHeadProps,
+  TableProps,
+  TableRowProps,
+} from "./table.types";
+
+export const Table: FC<TableProps> = ({ className, ...props }) => {
+  return (
+    <div
+      data-slot="table-container"
+      className="relative w-full overflow-x-auto"
+    >
+      <table
+        data-slot="table"
+        className={cn("w-full caption-bottom text-sm", className)}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export const TableHeader: FC<TableHeaderProps> = ({ className, ...props }) => {
+  return (
+    <thead
+      data-slot="table-header"
+      className={cn("[&_tr]:border-b border-blueprint-400", className)}
+      {...props}
+    />
+  );
+};
+
+export const TableBody: FC<TableBodyProps> = ({ className, ...props }) => {
+  return (
+    <tbody
+      data-slot="table-body"
+      className={cn("[&_tr:last-child]:border-0", className)}
+      {...props}
+    />
+  );
+};
+
+export const TableFooter: FC<TableFooterProps> = ({ className, ...props }) => {
+  return (
+    <tfoot
+      data-slot="table-footer"
+      className={cn(
+        "bg-muted/50 border-t font-medium [&>tr]:last:border-b-0",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export const TableRow: FC<TableRowProps> = ({ className, ...props }) => {
+  return (
+    <tr
+      data-slot="table-row"
+      className={cn(
+        "hover:bg-blueprint-900 data-[state=selected]:bg-muted border-b transition-colors",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export const TableHead: FC<TableHeadProps> = ({ className, ...props }) => {
+  return (
+    <th
+      data-slot="table-head"
+      className={cn(
+        "text-blueprint-100 h-10 px-2 text-left align-middle font-bold whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export const TableCell: FC<TableCellProps> = ({ className, ...props }) => {
+  return (
+    <td
+      data-slot="table-cell"
+      className={cn(
+        "p-2 align-middle text-blueprint-100 whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export const TableCaption: FC<TableCaptionProps> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <caption
+      data-slot="table-caption"
+      className={cn("text-blueprint-200/50 mt-4 text-sm", className)}
+      {...props}
+    />
+  );
+};
