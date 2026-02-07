2c859c2d821c00226baceaef11c4f7f6419c9c63
Daniel Buchal <daniel.buchal01@gmail.com>
2026-02-07 14:01:46 +0100

    feat: rewritten static scraper into more readable && scalable way (using `node-html-parser` instead of cheerio && `undici` instead of axios)

--- apps/api/src/lib/scrape.ts
+++ apps/api/src/lib/scrape.ts
@@ -1,14 +1,20 @@
-import { apiScraper } from "@scrapeek/scrapers/api";
-import { dynamicSiteScraper } from "@scrapeek/scrapers/dynamic";
-import { staticSiteScraper } from "@scrapeek/scrapers/static";
+import { apiScraper } from "@scrapeek/scrapers/legacy/api";
+import { dynamicSiteScraper } from "@scrapeek/scrapers/legacy/dynamic";
+import { staticSiteScraper } from "@scrapeek/scrapers/legacy/static";
 import type { Blueprint } from "@scrapeek/shared/blueprint";
 import { StatusError } from "@/lib/error.ts";
+import { StaticScraper } from "@scrapeek/scrapers/new/static";
 
 export const scrapeData = async (blueprints: Blueprint[], testRun = false) => {
   const scrapersToRun = blueprints.map((blueprint) => {
+    if (blueprint.type === "static") {
+      const staticScraper = new StaticScraper(blueprint, { isTestRun: testRun });
+      return staticScraper.scrape();
+
+      // return staticSiteScraper(blueprint,testRun)
+    }
     if (blueprint.type === "api") return apiScraper(blueprint, testRun);
-    if (blueprint.type === "dynamic")
-      return dynamicSiteScraper(blueprint, testRun);
+    if (blueprint.type === "dynamic") return dynamicSiteScraper(blueprint, testRun);
     return staticSiteScraper(blueprint, testRun);
   });
 
--- packages/scrapers/src/core/new/static.ts
+++ packages/scrapers/src/core/new/static.ts
@@ -1,8 +1,8 @@
+import type { IScraper, ScraperOptions } from "@/types";
+import { parseURL } from "@/utils/url";
 import type { StaticBlueprint } from "@scrapeek/shared/blueprint";
 import { parse } from "node-html-parser";
 import { request } from "undici";
-import type { IScraper, ScraperOptions } from "@/types";
-import { parseURL } from "@/utils/url";
 
 export class StaticScraper implements IScraper {
   blueprint: StaticBlueprint;
@@ -19,16 +19,59 @@ export class StaticScraper implements IScraper {
   async scrape() {
     try {
       const { config } = this.blueprint;
+      const { elements, pagination } = config;
       const parsedURL = parseURL(this.blueprint.url);
 
       const { body } = await request(parsedURL.originalUrl, { method: "GET" });
       const data = await body.text();
 
       const root = parse(data);
+
+      let scrapedData: Record<string, string>[] = [];
+
+      for (const { key, selector, attribute } of elements) {
+        const foundElements = root.querySelectorAll(selector);
+        // const isAttributeSearch = element.attribute !== undefined;
+
+        foundElements.forEach((element, index) => {
+          let value: string;
+          if (attribute !== undefined) {
+            value = element.getAttribute(attribute) ?? "";
+          } else {
+            value = element.textContent.trim();
+          }
+
+          if (!scrapedData[index]) scrapedData[index] = {};
+          scrapedData[index][key] = value;
+        });
+      }
+
+      if (pagination !== undefined) {
+        const { attribute, selector } = pagination;
+        const nextPageElement = root.querySelector(selector);
+        if (!nextPageElement) return scrapedData;
+
+        const nextPageLink = nextPageElement.getAttribute(attribute);
+        if (!nextPageLink) return scrapedData;
+
+        if (nextPageLink === this.blueprint.url) return scrapedData;
+
+        const newPageUrl = nextPageLink.startsWith("http")
+          ? nextPageLink
+          : `${parsedURL.protocol}://${parsedURL.domain}${nextPageLink}`;
+
+        this.blueprint = { ...this.blueprint, url: newPageUrl };
+
+        let newlyScrapedData = await this.scrape();
+
+        if (!newlyScrapedData) newlyScrapedData = [];
+
+        scrapedData = [...scrapedData, ...newlyScrapedData];
+      }
+
+      return scrapedData;
     } catch (err) {
       throw new Error((err as Error).message);
     }
-
-    return {};
   }
 }
