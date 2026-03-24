79be91e9049c56cba800fc974fb97170268250fb
Daniel Buchal <88269716+Bushenzie@users.noreply.github.com>
2026-03-24 11:56:35 +0100

    feat: Missing tooltip provider

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import type { QueryClient } from "@tanstack/react-query"
 import { createRootRouteWithContext, HeadContent, Scripts } from "@tanstack/react-router"
 import { Navbar } from "@/components/ui/navbar/navbar"
 import { Toaster } from "@/components/ui/toasts/toasts"
+import { TooltipProvider } from "@/components/ui/tooltip/tooltip"
 import { getSession } from "@/features/auth/utils/get-session"
 import css from "../styles.css?url"
 
@@ -48,12 +49,14 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body className="bg-blueprint-900 font-mono text-blueprint-100">
-        <Toaster />
-        <div className="w-full max-w-[1280px] space-y-4 h-full mx-auto px-4 py-8">
-          <Navbar />
-          {children}
-        </div>
-        <div className="absolute -z-10 inset-0 min-h-full w-full bg-[linear-gradient(to_right,var(--color-blueprint-700)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-blueprint-700)_1px,transparent_1px)] bg-[size:3.5rem_3.5rem]" />
+        <TooltipProvider>
+          <Toaster />
+          <div className="w-full max-w-[1280px] space-y-4 h-full mx-auto px-4 py-8">
+            <Navbar />
+            {children}
+          </div>
+          <div className="absolute -z-10 inset-0 min-h-full w-full bg-[linear-gradient(to_right,var(--color-blueprint-700)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-blueprint-700)_1px,transparent_1px)] bg-[size:3.5rem_3.5rem]" />
+        </TooltipProvider>
         <Scripts />
       </body>
     </html>
